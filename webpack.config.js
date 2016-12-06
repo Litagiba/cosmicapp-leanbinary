df7aee7b436df4c9b9ecf547a66f161db5637579
lilian.couto <lilian@heydancers>
2016-12-06 12:46:54 +0000

    added extract plugin, path definition but webpack-dev-server only serves the directory's listing instead of running the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,49 @@
-var path = require('path');
+// webpack.config.js
+const webpack = require('webpack')
+
+//Copy files
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, './app.js'),
+  build: path.join(__dirname, './public'),
+  cssLoc: path.join(__dirname, './assets/css'),
+  imagesLoc: path.join(__dirname, './images')
+};
 
 module.exports = {
-  devtool: 'eval',
-  entry: './app.js',
-  output: {
-    path: __dirname + '/public/',
-    filename: 'bundle.js'
+  debug: true,
+  devtool: "#eval-source-map",
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
+  //bundle app from here
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    publicPath: PATHS.build
+  },
+  devServer: {
+    // This is required for webpack-dev-server. The path should
+    // be an absolute path to your build destination.
+    outputPath: PATHS.build
+  },
+  devtool: "sourceMap", //or inline source-map
   module: {
     loaders: [
-      { test: /\.json$/, loader: 'json-loader' }
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+      }
     ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./assets/css")]
   },
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.js']
